e44d390014febca1b79466ebf301806ccc42c47c
SakuOrdrTab <leinosensaku@gmail.com>
2025-01-13 22:05:00 +0200

    metafiles updated

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="golf_card_game",
-    version="0.4",
+    version="0.4.1",
+    author="SakuOrdrTab",
+    description="RL agent training and Golf cardgame.",
+    long_description=open("README.md").read(),
+    url="https://github.com/SakuOrdrTab/golf_card_game",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
